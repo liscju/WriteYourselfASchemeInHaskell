506af94a6cff9ebf37850ae92c8f67cf365b3fe0
liscju <piotr.listkiewicz@gmail.com>
2015-07-24 01:26:19 +0200

    Done II chapter

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,71 @@
 module Main where
-import System.Environment
+
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+data LispVal = Atom String
+    | List [LispVal]
+    | DottedList [LispVal] LispVal
+    | Number Integer
+    | String String
+    | Bool Bool
+    deriving(Show)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString =
+    do
+        char '"'
+        x <- many (noneOf "\"")
+        char '"'
+        return $ String x
+
+
+parseAtom :: Parser LispVal
+parseAtom =
+    do
+        first <- letter <|> symbol
+        rest <- many (letter <|> digit <|> symbol)
+        let atom = first:rest
+        return $ case atom of
+            "#t" -> Bool True
+            "#f" -> Bool False
+            _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+    <|> parseString
+    <|> parseNumber
+
+readExpr :: String -> String
+readExpr input =
+    case parse parseExpr "lisp" input of
+        Left err -> "No match: " ++ show err
+        Right val -> "Found value: " ++ show val
 
 main :: IO ()
 main = do
     args <- getArgs
-    let (arg1,arg2) = (read $ args !! 0,read $ args !! 1) :: (Integer,Integer)
-    let result = arg1 + arg2
-    putStrLn ("Result = " ++ show result)
+    putStrLn (readExpr (args !! 0))
+
+
+
+
+
+
+
+
+
+
+
+
+
