25d7b2df6e300d9b420ccb217ecafa164c65e309
liscju <piotr.listkiewicz@gmail.com>
2015-08-02 13:26:34 +0200

    DOne whole chapter 6

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Control.Monad.Error
+import System.IO hiding (try)
 
 data LispVal = Atom String
     | List [LispVal]
@@ -240,11 +241,76 @@ readExpr input =
         Left err -> throwError $ Parser err
         Right val -> return val
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr =
+    return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m () ) -> m ()
+until_ pred prompt action =
+    do
+        result <- prompt
+        if pred result
+            then return ()
+            else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
+
 main :: IO ()
 main = do
     args <- getArgs
-    evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-    putStrLn $ extractValue $ trapError evaled
+    case length args of
+        0 -> runRepl
+        1 -> evalAndPrint $ args !! 0
+        otherwise -> putStrLn "Program takes only 0 or 1 argument"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
